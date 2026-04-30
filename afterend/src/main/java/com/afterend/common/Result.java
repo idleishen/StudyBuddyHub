f64d77565c1757d4a395379ee99304b25723c131
Rose <kongzjssss@163.com>
2026-04-30 17:23:15 +0800

    合并

--- afterend/src/main/java/com/afterend/common/Result.java
+++ afterend/src/main/java/com/afterend/common/Result.java
@@ -1,27 +1,19 @@
 package com.afterend.common;
+
+import lombok.Getter;
+
+@Getter
 public class Result<T> {
     private Integer code;
     private String message;
     private T data;
 
-    public Integer getCode() {
-        return code;
-    }
-
     public void setCode(Integer code) {
         this.code = code;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public T getData() {
-        return data;
     }
 
     public void setData(T data) {
